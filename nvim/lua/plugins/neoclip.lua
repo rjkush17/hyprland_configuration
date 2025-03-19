cf9c914c129318d96602b0a4b0050185823a2526
rjkush17 <rjkush17rishabh@gmail.com>
2025-03-19 21:55:35 +0530

    add new plugin

--- nvim/lua/plugins/neoclip.lua
+++ nvim/lua/plugins/neoclip.lua
@@ -21,24 +21,52 @@ return {
       vim.keymap.set('n', '<leader>fp', ':Telescope projects<CR>', { noremap = true, silent = true })
     end
   },
-{
-  "folke/flash.nvim",
-  event = "VeryLazy",
-  opts = {},
-  config = function()
-    require("flash").setup({
-      labels = "asdfghjklqwertyuiopzxcvbnm", -- Easy keys for jumping
-      modes = {
-        search = { enabled = true }, -- `/` ke saath flash enable
-        char = { enabled = true },   -- Sneak-like motion
-        treesitter = { enabled = true }, -- Better syntax jumps
-      }
-    })
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    config = function()
+      require("flash").setup({
+        labels = "asdfghjklqwertyuiopzxcvbnm", -- Easy keys for jumping
+        modes = {
+          search = { enabled = true },         -- `/` ke saath flash enable
+          char = { enabled = true },           -- Sneak-like motion
+          treesitter = { enabled = true },     -- Better syntax jumps
+        }
+      })
 
-    -- Keymaps for Flash.nvim
-    vim.keymap.set({ "n", "x", "o" }, "s", function() require("flash").jump() end, { noremap = true, silent = true })
-    vim.keymap.set({ "n", "o", "x" }, "S", function() require("flash").treesitter() end, { noremap = true, silent = true })
-    vim.keymap.set({ "o" }, "<C-e>", function() require("flash").remote() end, { noremap = true, silent = true })
-  end
-}
+      -- Keymaps for Flash.nvim
+      vim.keymap.set({ "n", "x", "o" }, "s", function() require("flash").jump() end, { noremap = true, silent = true })
+      vim.keymap.set({ "n", "o", "x" }, "S", function() require("flash").treesitter() end,
+        { noremap = true, silent = true })
+      vim.keymap.set({ "o" }, "<C-e>", function() require("flash").remote() end, { noremap = true, silent = true })
+    end
+  },
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async" },
+    config = function()
+      vim.o.foldcolumn = '1' -- Show fold column
+      vim.o.foldlevel = 99
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      -- Hide fold level numbers in the fold column
+      vim.o.fillchars = "eob: ,fold: ,foldopen:,foldclose:"
+
+
+      -- Folding Keybindings
+      vim.keymap.set('n', 'zR', require('ufo').openAllFolds, { desc = "Open all folds" })
+      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds, { desc = "Close all folds" })
+
+      vim.keymap.set('n', '<leader>z', 'za', { desc = "Toggle Fold" })
+
+      -- Setup ufo with Treesitter & LSP
+      require('ufo').setup({
+        provider_selector = function(bufnr, filetype, buftype)
+          return { 'treesitter', 'indent' }
+        end
+      })
+    end
+  },
 }
